d2033c30bd7bcad26868a36e3a35999964a9d85b
Tenacc <tagoba.almeida@gmail.com>
2022-02-07 15:34:14 -0300

    feat: add pesquisarDespesa function

--- app.js
+++ app.js
@@ -142,4 +142,23 @@ function carregaListaDespesas() {
     })
 }
 
+function pesquisarDespesa () {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value 
+    let tipo = document.getElementById('tipo').value 
+    let descricao = document.getElementById('descricao').value 
+    let valor = document.getElementById('valor').value 
+
+    
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+
+
+    console.log(despesa)
+
+
+}
+
 
